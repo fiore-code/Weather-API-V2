5296354b78b52ce7bc6eb66d376b1de1d541acff
suparna biswas <subhambiswas38@gmail.com>
2021-08-22 23:45:38 +0530

    updating database file

--- configuration/database.js
+++ configuration/database.js
@@ -1,8 +1,8 @@
 module.exports={
-    databaseType:'mysql',
-    username:'subham',
-    password:'NotWelcome1234',
-    host:'subhamdb.c80a2jnld8z1.ap-south-1.rds.amazonaws.com',
+    databaseType:'xxxxx',
+    username:'xxxxx',
+    password:'xxxxxxxxxxxx',
+    host:'xxxxxxxxxxxxxxxxxxxx.amazonaws.com',
     port:'3306',
-    databaseName:'WeatherDB'
+    databaseName:'xxxxxxxxxxxx'
 }
